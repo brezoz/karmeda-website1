d6868973f4a0525c559f3d834757049ef5f32add
brezoz <marchel.adisastra@gmail.com>
2026-05-02 19:52:24 +0700

    Update stat from 50 to 50+ penjahit ahli

--- components/hero.jsx
+++ components/hero.jsx
@@ -142,7 +142,7 @@ function HeroCatalog({ t }) {
 function HeroStats({ t, dark }) {
   const stats = [
     { n: '20+', l: t('stat_years') },
-    { n: '50', l: t('stat_employees') },
+    { n: '50+', l: t('stat_employees') },
     { n: '200+', l: t('stat_clients') },
     { n: '10K', l: t('stat_pieces') },
   ];
